ac9126671f2196711cd60939592725e011f26572
EmreKayal <54174956+ByaCherX@users.noreply.github.com>
2021-07-04 00:36:59 +0300

    Development Version Created (dev.cpp)

---
+++ dev.cpp
@@ -0,0 +1,14 @@
+/* This file Development version C++ code */
+
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int64_t BranchCommit_SHA_Key = 0000000;
+
+int main()
+{
+    void GitCommit(int key);
+    string Commit_LFS = "commit-1234567";
+}
