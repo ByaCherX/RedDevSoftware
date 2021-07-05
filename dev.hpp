1c3330e6fac3e48ffbbbbee56f5cd8f6824b8761
EmreKayal <54174956+ByaCherX@users.noreply.github.com>
2021-07-05 11:22:06 +0300

    dev.hpp update

---
+++ dev.hpp
@@ -0,0 +1,13 @@
+#pragma region TR
+
+#include <cstdlib>
+#include <ios>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 8;
+    string devCommit = "4f8c9a3wf 08rc0rx6awr5 08rawer10ca6w2c8et 05a 0wer6c06 57419795";
+}
