18ee5fe403432d53fea9d05a5a413117fc6f041a
EmreKayal <54174956+ByaCherX@users.noreply.github.com>
2021-07-05 11:45:32 +0300

    dev.hpp change

--- dev.hpp
+++ dev.hpp
@@ -9,5 +9,5 @@ using namespace std;
 int main()
 {
     int x = 8;
-    string devCommit = "4f8c9a3wf 08rc0rx6awr5 08rawer10ca6w2c8et 05a 0wer6c06 57419795";
+    string devCommit = "commit unable - warning !!!";
 }
